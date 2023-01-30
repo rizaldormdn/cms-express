a4ab250b3374d8722a9e7b22394a916e74d6617f
ari-salt <aristo.rinjuang@salt.co.id>
2023-01-30 20:26:52 +0700

    feat: valueobject; Dimension

--- src/valueobject/Dimension.ts
+++ src/valueobject/Dimension.ts
@@ -1,18 +1,21 @@
 export default class Dimension {
-	private _height: number;
-	private _width: number;
+  private _height: number;
+  private _width: number;
 
-    constructor(height: number, width: number) {
-        this._height = height;
-        this._width = width;
+  constructor(height: number, width: number) {
+    if (height <= 0 || width <= 0) {
+      throw new Error('dimension is invalid')
     }
+  
+    this._height = height;
+    this._width = width;
+  }
 
-       public get height() {
-            return this._height;
-        }
+  public get height() {
+    return this._height;
+  }
 
-       public get width() {
-            return this._width;
-        }
-
+  public get width() {
+    return this._width;
+  }
 }
