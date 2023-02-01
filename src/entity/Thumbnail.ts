997078eb98225248a0e0f5f9443b43be5c37424c
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-02 00:58:40 +0700

    fix: Thumbnail

--- src/entity/Thumbnail.ts
+++ src/entity/Thumbnail.ts
@@ -1,29 +1,30 @@
 import { v4 as uuidv4 } from "uuid";
 import ThumbnailCategory from "../entity/ThumbnailCategory";
-import Image from "../entity/Image";
 
 export default class Thumbnail {
-  public _id: string;
-  public _url: string;
-  public _category: ThumbnailCategory;
-  public _belongToImage: Image;
+  private _id: string;
+  private _url: string;
+  private _category: ThumbnailCategory;
 
-  constructor(url: string, category: ThumbnailCategory, belongToImage: Image) {
+  constructor(url: string, category: ThumbnailCategory) {
+    if (url === "") {
+      throw new Error("url cannot be empty")
+    }
+
     this._id = uuidv4();
     this._url = url;
     this._category = category;
-    this._belongToImage = belongToImage;
   }
-  get id(): string {
+
+  public get id(): string {
     return this._id;
   }
-  get url(): string {
+
+  public get url(): string {
     return this._url;
   }
-  get category(): ThumbnailCategory {
+
+  public get category(): ThumbnailCategory {
     return this._category;
-  }
-  get belongToImage(): Image {
-    return this._belongToImage;
   }
 }
