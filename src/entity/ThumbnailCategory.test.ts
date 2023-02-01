975e441a15433f41cf7c41c18367a613f9406994
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-02 00:56:01 +0700

    fix: ThumbnailCategory

--- src/entity/ThumbnailCategory.test.ts
+++ src/entity/ThumbnailCategory.test.ts
@@ -1,10 +1,24 @@
 import ThumbnailCategory from "./ThumbnailCategory";
 import Dimension from "../valueobject/Dimension";
 
-describe("ThumbnailCategory", () => {
-    it("Throw Error When Name Is Empty", () => {
-        expect( () => {
-            new ThumbnailCategory("", new Dimension(100,100)) 
-        }).toThrowError("Name Is Required")
+describe("thumbnail category", () => {
+    let thumbnailCategory: ThumbnailCategory = new ThumbnailCategory(
+        "Profile Picture",
+        new Dimension(100, 100)
+    );
+
+    it("should have id", () => {
+        expect(thumbnailCategory.id).not.toEqual("")
+    })
+
+    it("should have name", () => {
+        expect(thumbnailCategory.name).toEqual("Profile Picture")
+        expect(() => {
+            new ThumbnailCategory("", new Dimension(100, 100)) 
+        }).toThrowError("name cannot be empty")
+    })
+
+    it("should have dimension", () => {
+        expect(thumbnailCategory.dimension).toBe(new Dimension(100, 100))
     })
 })
