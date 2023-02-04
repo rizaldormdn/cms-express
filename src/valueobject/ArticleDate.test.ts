7629cd42e6ed4974aa68135e98b7e878425d5835
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-04 16:35:49 +0700

    fix: value object; ArticleDate update to current datetime

--- src/valueobject/ArticleDate.test.ts
+++ src/valueobject/ArticleDate.test.ts
@@ -1,27 +1,35 @@
 import ArticleDate from './ArticleDate'
 
 describe('vlaue object ArticleDate', () => {
-     const articleDate: ArticleDate = new ArticleDate()
+  let articleDate: ArticleDate = new ArticleDate()
 
-     it('should set createdAt to current date by default', () => {
-          expect(articleDate.createdAt).toBeInstanceOf(Date)
-     })
-     it('should set updatedAt to current date by default', () => {
-          expect(articleDate.updatedAt).toBeInstanceOf(Date)
-     })
-     it('should set createdAt to provided date', () => {
-          const createdAt = new Date('2023-01-01')
-          const articleDate = new ArticleDate(createdAt)
-          expect(articleDate.createdAt).toEqual(createdAt)
-     })
-     it('should set updatedAt to provided date', () => {
-          const updatedAt = new Date('2023-01-01')
-          const articleDate = new ArticleDate(undefined, updatedAt)
-          expect(articleDate.updatedAt).toEqual(updatedAt)
-     })
-     it('should update updatedAt', () => {
-          const updatedAt = new Date('2023-01-01')
-          articleDate.update(updatedAt)
-          expect(articleDate.updatedAt).toEqual(updatedAt)
-     })
+  it('should set createdAt to current date by default', () => {
+    expect(articleDate.createdAt).toBeInstanceOf(Date)
+  })
+
+  it('should set updatedAt to current date by default', () => {
+      expect(articleDate.updatedAt).toBeInstanceOf(Date)
+  })
+
+  it('should set createdAt to provided date', () => {
+    let createdAt = new Date('2023-01-01')
+    let articleDate = new ArticleDate(createdAt)
+
+    expect(articleDate.createdAt).toEqual(createdAt)
+  })
+
+  it('should set updatedAt to provided date', () => {
+    let updatedAt = new Date('2023-01-01')
+    let articleDate = new ArticleDate(undefined, updatedAt)
+    
+    expect(articleDate.updatedAt).toEqual(updatedAt)
+  })
+
+  it('should update updatedAt', () => {
+    let previousDate = articleDate.updatedAt
+
+    articleDate.update()
+
+    expect(articleDate.updatedAt).not.toEqual(previousDate)
+  })
 })
--- src/valueobject/ArticleDate.ts
+++ src/valueobject/ArticleDate.ts
@@ -6,13 +6,16 @@ export default class ArticleDate {
 		this._createdAt = createdAt || new Date();
 		this._updatedAt = updatedAt || new Date();
 	}
+
 	public get createdAt(): Date {
 		return this._createdAt;
 	}
+
 	public get updatedAt(): Date {
 		return this._updatedAt;
 	}
-	public update(updatedAt: Date): void {
-		this._updatedAt = updatedAt;
+
+	public update(): void {
+		this._updatedAt = new Date();
 	}
 }
