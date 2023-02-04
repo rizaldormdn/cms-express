98816480bd71dc135853e10bc5b9654e06e9c5d2
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-04 20:47:02 +0700

    feat: aggregate; Administrator

---
+++ src/aggregate/Administrator.test.ts
@@ -0,0 +1,19 @@
+import Email from "../valueobject/Email";
+import Name from "../valueobject/Name";
+import Password from "../valueobject/Password";
+import Administrator from "./Administrator";
+import User from "./User";
+
+describe("Administator", () => {
+  let email: Email = new Email("test@example.com");
+  let name: Name = new Name("John Doe");
+  let password: Password = new Password("$2b$10$WCZ6j4PLICecyCYvBvL7We");
+  let administator: Administrator = new Administrator(email, name, password);
+
+  it("should create an author", () => {
+    let authorEmail: Email = new Email("author@example.com");
+    let user: User = new User(authorEmail, name, password);
+    
+    expect(administator.createAuthor(user)).toBeDefined()
+  })
+})
--- src/aggregate/Administrator.ts
+++ src/aggregate/Administrator.ts
@@ -1,20 +1,19 @@
 import Author from "./Author";
 import User from "./User";
-import { Articles } from "../entity/Article";
+import Email from "../valueobject/Email";
+import Name from "../valueobject/Name";
+import Password from "../valueobject/Password";
 
-class Administator {
-  private _authors: Authors;
-
-  constructor(authors: Authors) {
-    this._authors = authors;
+export default class Administrator extends User {
+  constructor(
+    email: Email,
+    name: Name,
+    password: Password
+  ) {
+    super(email, name, password);
   }
 
-  public registerAuthor(article: Articles, user: User): Author {
-    let author = new Author(article, user.email, user.name, user.password);
-    return author;
+  public createAuthor(user: User): Author {
+    return new Author(user.email, user.name, user.password, []);
   }
 }
-
-export default Administator;
-
-export type Authors = Authors[];
