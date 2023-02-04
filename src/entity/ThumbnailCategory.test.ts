c57938e13c725d38b11c37ddd4d1c2644b966bf2
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-04 16:42:46 +0700

    refactor: entity; ThumbnailCategory

--- src/entity/ThumbnailCategory.test.ts
+++ src/entity/ThumbnailCategory.test.ts
@@ -2,23 +2,22 @@ import ThumbnailCategory from "./ThumbnailCategory";
 import Dimension from "../valueobject/Dimension";
 
 describe("thumbnail category", () => {
-    let thumbnailCategory: ThumbnailCategory = new ThumbnailCategory(
-        "Profile Picture",
-        new Dimension(100, 100)
-    );
+  let dimension: Dimension = new Dimension(100, 100)
+  let thumbnailCategory: ThumbnailCategory = new ThumbnailCategory(
+    "Profile Picture",
+    dimension
+  );
 
-    it("should have id", () => {
-        expect(thumbnailCategory.id).not.toEqual("")
-    })
+  it("should have id", () => {
+    expect(thumbnailCategory.id).not.toEqual("")
+  })
 
-    it("should have name", () => {
-        expect(thumbnailCategory.name).toEqual("Profile Picture")
-        expect(() => {
-            new ThumbnailCategory("", new Dimension(100, 100)) 
-        }).toThrowError("name cannot be empty")
-    })
+  it("should have name", () => {
+    expect(thumbnailCategory.name).toEqual("Profile Picture")
+    expect(() => new ThumbnailCategory("", dimension)).toThrowError("name cannot be empty")
+  })
 
-    it("should have dimension", () => {
-        expect(thumbnailCategory.dimension).toBe(new Dimension(100, 100))
-    })
+  it("should have dimension", () => {
+    expect(thumbnailCategory.dimension).toBe(dimension)
+  })
 })
--- src/entity/ThumbnailCategory.ts
+++ src/entity/ThumbnailCategory.ts
@@ -2,29 +2,29 @@ import { v4 as uuidv4 } from "uuid";
 import Dimension from "../valueobject/Dimension";
 
 export default class ThumbnailCategory {
-    private _id: string;
-    private _name: string;
-    private _dimension: Dimension;
+  private _id: string;
+  private _name: string;
+  private _dimension: Dimension;
 
-    constructor(name: string, dimension: Dimension) {
-        if (name === "") {
-            throw new Error("name cannot be empty")
-        }
-
-        this._id = uuidv4();
-        this._name = name;
-        this._dimension = dimension;
+  constructor(name: string, dimension: Dimension) {
+    if (name === "") {
+      throw new Error("name cannot be empty")
     }
 
-    public get id(): string {
-        return this._id;
-    }
+    this._id = uuidv4();
+    this._name = name;
+    this._dimension = dimension;
+  }
 
-    public get name(): string {
-        return this._name;
-    }
+  public get id(): string {
+    return this._id;
+  }
 
-    public get dimension(): Dimension {
-        return this._dimension;
-    }
+  public get name(): string {
+    return this._name;
+  }
+
+  public get dimension(): Dimension {
+    return this._dimension;
+  }
 }
