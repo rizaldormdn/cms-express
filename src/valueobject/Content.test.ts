1981d03dac1d0341e5fe5d21652bf199aabee7ba
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-04 16:20:29 +0700

    test: value object; Content

--- src/valueobject/Content.test.ts
+++ src/valueobject/Content.test.ts
@@ -1,5 +1,11 @@
 import Content from "./Content";
 
+type content = {
+  title: string
+  content: string
+  excerpt: string
+}
+
 describe("content value object", () => {
   let content: Content = new Content("Jalan-jalan", "ke pantai", "Liburan");
 
@@ -10,7 +16,36 @@ describe("content value object", () => {
   test('content of "ke pantai" should be "ke pantai"', () => {
     expect(content.content).toBe("ke pantai");
   });
+
   test('excerpt of "Liburan" should be "Liburan"', () => {
     expect(content.excerpt).toBe("Liburan");
   });
+
+  test('title, content, and excerpt are required', () => {
+    let invalidContents: content[] = [
+      {
+        title: '',
+        content: 'This is content.',
+        excerpt: 'This is excerpt.'
+      },
+      {
+        title: 'This is title',
+        content: '',
+        excerpt: 'This is excerpt.'
+      },
+      {
+        title: 'This is title',
+        content: 'This is content.',
+        excerpt: ''
+      },
+    ]
+
+    for (let invalidContent of invalidContents) {
+      expect(() => new Content(
+        invalidContent.title,
+        invalidContent.content,
+        invalidContent.excerpt
+      )).toThrowError()
+    }
+  })
 });
