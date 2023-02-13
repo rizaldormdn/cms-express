33d2eba5772eb963884ab8e53077149fa847c793
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-13 21:58:06 +0700

    fix: delete ThumbnailCategoryRepository

--- src/domain/repository/ThumbnailCategoryRepository.ts
+++
@@ -1,7 +0,0 @@
-import ThumbnailCategory, { ThumbnailCategories } from '../entity/ThumbnailCategory'
-
-export default interface ThumbnailCategoryRepository{
-  getThumbnailCategories(): Promise<ThumbnailCategories>
-  saveThumbnailCategory(thumbnailCategory: ThumbnailCategory): Promise<void>
-  deleteThumbnailCategory(uuid: string): Promise<void>
-}
