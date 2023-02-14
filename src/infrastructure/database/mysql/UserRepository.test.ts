b28464af568ca0a4260e1eb3527e490be7b7e9e4
alvito <alvitozidane76@gmail.com>
2023-02-15 02:37:45 +0700

    feat:infrastructure; saveAuthor

--- src/infrastructure/database/mysql/UserRepository.test.ts
+++ src/infrastructure/database/mysql/UserRepository.test.ts
@@ -1,64 +1,111 @@
 import mysql, { Connection } from "mysql2";
 import sinon, { SinonMock } from "sinon";
 import * as UserRepositoryDomain from "../../../domain/repository/UserRepository";
-import Email from "../../../domain/valueobject/Email";
 import UserRepository from "./UserRepository";
 
+import Author from "../../../domain/entity/Author";
+import Email from "../../../domain/valueobject/Email";
+import Name from "../../../domain/valueobject/Name";
+import Password from "../../../domain/valueobject/Password";
+
 describe("User Repository MySQL", () => {
-	let connection: Connection = mysql.createConnection({ host: "localhost" });
-	let mock: SinonMock = sinon.mock(connection);
-	let repository: UserRepositoryDomain.default = new UserRepository(connection);
-	let email: Email = new Email("admin@example.com");
+  let connection: Connection = mysql.createConnection({
+    host: "localhost",
+  });
+  let mock: SinonMock = sinon.mock(connection);
+  let repository: UserRepositoryDomain.default = new UserRepository(connection);
+  let email: Email = new Email("admin@example.com");
+  let name: Name = new Name("author", "last");
+  let password: Password = new Password("$2b$10$z1e0ySIYbA/5FXNzZy.Qge");
+  let author: Author = new Author(email, name, password);
 
-	it("should return an administrator", async () => {
-		mock
-			.expects("query")
-			.once()
-			.withArgs("SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1")
-			.callsArgWith(
-				2,
-				null,
-				[
-					{
-						first_name: "Admin",
-						last_name: "",
-						salt: "$2b$10$z1e0ySIYbA/5FXNzZy.Qge",
-						hashed_password: "$2b$10$z1e0ySIYbA/5FXNzZy.Qgefti4GC8YwobSbO81EfD9JTuX/X1J1Xu"
-					}
-				],
-				["first_name", "last_name", "salt", "hashed_password"]
-			);
+  it("should return an administrator", async () => {
+    mock
+      .expects("query")
+      .once()
+      .withArgs(
+        "SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1"
+      )
+      .callsArgWith(
+        2,
+        null,
+        [
+          {
+            first_name: "Admin",
+            last_name: "",
+            salt: "$2b$10$z1e0ySIYbA/5FXNzZy.Qge",
+            hashed_password:
+              "$2b$10$z1e0ySIYbA/5FXNzZy.Qgefti4GC8YwobSbO81EfD9JTuX/X1J1Xu",
+          },
+        ],
+        ["first_name", "last_name", "salt", "hashed_password"]
+      );
 
-		expect(await repository.getAdministrator(email)).toBeDefined()
-	});
+    expect(await repository.getAdministrator(email)).toBeDefined();
+  });
 
-	it("should return an error if failed get an administrator", async () => {
-		mock.expects("query").once().callsArgWith(2, new Error(), null, null);
+  it("should return an error if failed get an administrator", async () => {
+    mock.expects("query").once().callsArgWith(2, new Error(), null, null);
 
-		try {
-			await repository.getAdministrator(email);
-		} catch (err) {
-			expect(err).toBeInstanceOf(Error)
-		}
-	})
+    try {
+      await repository.getAdministrator(email);
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
 
-	// it("should delete an author", async () => {
-	// 	mock.expects("query").once().withArgs("DELETE FROM users")
+  it("should return an insert/save author", async () => {
+    mock
+      .expects("query")
+      .once()
+      .withArgs(
+        "INSERT INTO users (email, first_name, last_name, salt, hashed_password, is_administrator) VALUES (?, ?, ?, ?, ?)"
+      )
+      .callsArgWith(
+        2,
+        null,
+        [
+          {
+            email: author.email.string(),
+            first_name: author.name.first,
+            last_name: author.name.last,
+            salt: author.password.salt,
+            hashed_password: author.password.hashedPassword,
+            is_administrator: "FALSE",
+          },
+        ],
+        ["first_name", "last_name", "salt", "hashed_password", "email"]
+      );
+    expect(await repository.saveAuthor(author)).toBeDefined();
+  });
 
-	// 	try {
-	// 		await repository.deleteAuthor(email)
-	// 	} catch(err) {
-	// 		expect(err).toBeUndefined()
-	// 	}
-	// })
-	
-	// it("should return an error if failed delete an author", async () => {
-	// 	mock.expects("query").once().callArgWith(2, new Error(), null, null);
-	
-	// 	try {
-	// 		await repository.deleteAuthor(email)
-	// 	} catch (err) {
-	// 		expect(err).toBeInstanceOf(Error)
-	// 	}
-	// })
+  it("should return an error if failed get an insert/save author", async () => {
+    mock.expects("query").once().callsArgWith(2, new Error(), null, null);
+
+    try {
+      await repository.saveAuthor(author);
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
+
+  // it("should delete an author", async () => {
+  // 	mock.expects("query").once().withArgs("DELETE FROM users")
+
+  // 	try {
+  // 		await repository.deleteAuthor(email)
+  // 	} catch(err) {
+  // 		expect(err).toBeUndefined()
+  // 	}
+  // })
+
+  // it("should return an error if failed delete an author", async () => {
+  // 	mock.expects("query").once().callArgWith(2, new Error(), null, null);
+
+  // 	try {
+  // 		await repository.deleteAuthor(email)
+  // 	} catch (err) {
+  // 		expect(err).toBeInstanceOf(Error)
+  // 	}
+  // })
 });
