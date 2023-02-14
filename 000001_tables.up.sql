b3a085e1f6269c54833e7363e2f16a3b1d5d3099
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-14 10:35:18 +0700

    fix: change primary key for tables.sql

--- 000001_tables.up.sql
+++ 000001_tables.up.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
   `email` varchar(256) NOT NULL,
   `first_name` varchar(256) NOT NULL,
   `last_name` varchar(256) NOT NULL,
@@ -8,14 +7,12 @@ CREATE TABLE IF NOT EXISTS `users` (
   `is_administrator` boolean,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE (`email`),
+  PRIMARY KEY (`email`),
   INDEX `idx_is_administrator` (`is_administrator`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `images` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `uuid` binary(16) NOT NULL,
+  `id` binary(16) NOT NULL,
   `original_url` varchar(512) NOT NULL,
   `thumbnail_url` varchar(512) NOT NULL,
   `alt` varchar(256) NOT NULL,
@@ -23,25 +20,22 @@ CREATE TABLE IF NOT EXISTS `images` (
   `width` int unsigned NOT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE (`uuid`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 CREATE TABLE IF NOT EXISTS `articles` (
-  `id` int NOT NULL AUTO_INCREMENT,
   `slug` varchar(512) NOT NULL,
   `title` varchar(256) NOT NULL,
   `content` text NOT NULL,
   `excerpt` varchar(160) NOT NULL,
-  `image_id` int NOT NULL,
-  `author_id` int NOT NULL,
+  `image_id` binary(16) NOT NULL,
+  `author_email` varchar(256) NOT NULL,
   `tags` varchar(128),
   `is_published` boolean,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE (`slug`),
+  PRIMARY KEY (`slug`),
   FOREIGN KEY (`image_id`) REFERENCES `images`(`id`),
-  FOREIGN KEY (`author_id`) REFERENCES `users`(`id`),
+  FOREIGN KEY (`author_email`) REFERENCES `users`(`email`),
   INDEX `idx_is_published` (`is_published`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
