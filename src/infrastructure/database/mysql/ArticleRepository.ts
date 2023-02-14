4b8251e629d5b4840a7201d5141b1a38f980e2a9
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-15 02:56:34 +0700

    feat: ArticleRepository.getArticle()

--- src/infrastructure/database/mysql/ArticleRepository.ts
+++ src/infrastructure/database/mysql/ArticleRepository.ts
@@ -10,12 +10,155 @@ import Name from "../../../domain/valueobject/Name";
 import Tag, { Tags } from "../../../domain/valueobject/Tag";
 import ArticleDate from "../../../domain/valueobject/ArticleDate";
 import Specification from "../../../application/valueobject/Specification";
+import ImageURL from "../../../domain/valueobject/ImageURL";
+import Dimension from "../../../domain/valueobject/Dimension";
 
 export default class ArticleRepository implements ArticleRepositoryInterface.default {
   private _connection: Connection;
 
   constructor(connection: Connection) {
     this._connection = connection;
+  }
+
+  private _getArticle(slug: Slug): Promise<Article> {
+    return new Promise<Article>((resolve, reject) => {
+      let query = `
+        SELECT
+          slug,
+          title,
+          content,
+          excerpt,
+          BIN_TO_UUID(images.id) AS image_id,
+          original_url,
+          thumbnail_url,
+          alt,
+          height,
+          width,
+          first_name,
+          last_name,
+          tags,
+          is_published,
+          articles.created_at,
+          articles.updated_at
+        FROM articles
+        JOIN images ON images.id = articles.image_id
+        JOIN users ON users.email = articles.author_email
+        WHERE slug = ? LIMIT 1
+      `
+
+      this._connection.query(
+        query,
+        [slug.value],
+        (err: any | null, result: any) => {
+          if (err) {
+            console.error(err);
+
+            reject(err);
+          }
+          if (result.length > 0) {
+            let tags: Tags = []
+            for (let tag of result[0].tags.split(',')) {
+              tags.push(new Tag(tag))
+            }
+
+            resolve(new Article(
+              slug,
+              new Content(result[0].title, result[0].content, result[0].excerpt),
+              new Image(
+                new ImageURL(result[0].original_url, result[0].thumbnail_url),
+                result[0].alt,
+                new Dimension(result[0].height, result[0].width),
+                result[0].image_id
+              ),
+              new Name(result[0].first_name, result[0].last_name).full(), 
+              tags,
+              [],
+              result[0].is_published,
+              new ArticleDate(result[0].created_at, result[0].updated_at)
+            ))
+          }
+        }
+      )
+    })
+  }
+
+  private _getRelatedArticle(article: Article): Promise<ArticleSnapshots> {
+    return new Promise<ArticleSnapshots>((resolve, reject) => {
+      let tagsQuery = ''
+      let tagValues: string[] = []
+
+      for (let i = 0; i < article.tags.length; i++) {
+        if (tagsQuery !== '') {
+          tagsQuery += ' OR '
+        }
+        tagsQuery += 'tags LIKE ?'
+
+        if (i <= 0) {
+          tagValues.push(`${article.tags[i].value},%`)
+        } else if (i >= article.tags.length - 1) {
+          tagValues.push(`%,${article.tags[i].value}`)
+        } else {
+          tagValues.push(`%,${article.tags[i].value},%`)
+        }
+      }
+  
+      let query = `
+        SELECT
+          slug,
+          title,
+          excerpt,
+          thumbnail_url,
+          first_name,
+          last_name,
+          tags,
+          articles.created_at,
+          articles.updated_at
+        FROM articles
+        JOIN images ON images.id = articles.image_id
+        JOIN users ON users.email = articles.author_email
+        WHERE slug != ? AND (${tagsQuery})
+        ORDER BY articles.updated_at DESC LIMIT ?
+      `
+
+      this._connection.query(
+        query,
+        [article.slug.value, ...tagValues, Number(process.env.LIMIT_RELATED_ARTICLES)],
+        (err: any | null, result: any) => {
+          if (err) {
+            console.error(err);
+
+            reject(err);
+          }
+          if (result.length > 0) {
+            let relatedArticles: ArticleSnapshots = []
+
+            for (let entry of result) {
+              let tags: Tags = []
+              for (let tag of entry.tags.split(',')) {
+                tags.push(new Tag(tag))
+              }
+  
+              let relatedArticle = new ArticleSnapshot(
+                new Slug().rebuild(entry.slug),
+                entry.title,
+                entry.excerpt,
+                entry.thumbnail_url,
+                new Name(entry.first_name, entry.last_name).full(),
+                tags,
+                new ArticleDate(
+                  new Date(entry.created_at),
+                  new Date(entry.updated_at)
+                )
+              )
+    
+              relatedArticles.push(relatedArticle)
+            }
+
+            resolve(relatedArticles)
+          }
+        }
+      )
+    })
   }
 
   getFeaturedArticles(): Promise<ArticleSnapshots> {
@@ -41,7 +184,11 @@ export default class ArticleRepository implements ArticleRepositoryInterface.def
         query,
         [Number(process.env.LIMIT_FEATURED_ARTICLES)],
         (err: any | null, result: any) => {
-          if (err) reject(err);
+          if (err) {
+            console.error(err);
+
+            reject(err);
+          }
           if (result.length > 0) {
             let featuredArticles: ArticleSnapshots = []
 
@@ -100,7 +247,11 @@ export default class ArticleRepository implements ArticleRepositoryInterface.def
         query,
         [`%${specification.search}%`, offset, limit],
         (err: any | null, result: any) => {
-          if (err) reject(err);
+          if (err) {
+            console.error(err);
+  
+            reject(err);
+          }
           if (result.length > 0) {
             let featuredArticles: ArticleSnapshots = []
 
@@ -159,7 +310,11 @@ export default class ArticleRepository implements ArticleRepositoryInterface.def
         query,
         [`%${specification.search}%`, author.email.string(), offset, limit],
         (err: any | null, result: any) => {
-          if (err) reject(err);
+          if (err) {
+            console.error(err);
+
+            reject(err);
+          }
           if (result.length > 0) {
             let featuredArticles: ArticleSnapshots = []
 
@@ -193,25 +348,24 @@ export default class ArticleRepository implements ArticleRepositoryInterface.def
   }
 
   getArticle(slug: Slug): Promise<Article> {
-    return new Promise<Article>((resolve, reject) => {
-      this._connection.query(
-        "SELECT slug, content, image, author, tags, relatedArticles, date FROM article WHERE slug = ?",
-        [slug.value],
-        (err: any | null, result: any) => {
-          if (err) reject(err);
-          if (result.length > 0) {
-            resolve(new Article(
-              slug,
-              new Content(result.title, result.content, result.excerpt),
-              new Image(result.url, result.alt, result.dimension),
-              new Author(result[0].email, result[0].name, result[0].password) as any, 
-              result.tags,
-              result.relatedArticles,
-              result.date
-            ))
-          }
-        }
-      )
+    return new Promise<Article>(async (resolve, reject) => {
+      try {
+        let article = await this._getArticle(slug)
+        let relatedArticles = await this._getRelatedArticle(article)
+
+        resolve(new Article(
+          article.slug,
+          article.content,
+          article.image,
+          article.authorName,
+          article.tags,
+          relatedArticles,
+          article.isPublished,
+          article.date,
+        ))
+      } catch(err) {
+        reject(err);
+      }
     });
   }
 
