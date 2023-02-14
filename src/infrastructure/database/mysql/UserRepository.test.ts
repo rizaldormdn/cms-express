a168967d0bab0fcc52d651f4023941560ca48367
alvito <alvitozidane76@gmail.com>
2023-02-15 01:40:30 +0700

    feat:infrastructure; getAuthor

--- src/infrastructure/database/mysql/UserRepository.test.ts
+++ src/infrastructure/database/mysql/UserRepository.test.ts
@@ -5,60 +5,98 @@ import Email from "../../../domain/valueobject/Email";
 import UserRepository from "./UserRepository";
 
 describe("User Repository MySQL", () => {
-	let connection: Connection = mysql.createConnection({ host: "localhost" });
-	let mock: SinonMock = sinon.mock(connection);
-	let repository: UserRepositoryDomain.default = new UserRepository(connection);
-	let email: Email = new Email("admin@example.com");
+  let connection: Connection = mysql.createConnection({ host: "localhost" });
+  let mock: SinonMock = sinon.mock(connection);
+  let repository: UserRepositoryDomain.default = new UserRepository(connection);
+  let email: Email = new Email("admin@example.com");
 
-	it("should return an administrator", async () => {
-		mock
-			.expects("query")
-			.once()
-			.withArgs("SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1")
-			.callsArgWith(
-				2,
-				null,
-				[
-					{
-						first_name: "Admin",
-						last_name: "",
-						salt: "$2b$10$z1e0ySIYbA/5FXNzZy.Qge",
-						hashed_password: "$2b$10$z1e0ySIYbA/5FXNzZy.Qgefti4GC8YwobSbO81EfD9JTuX/X1J1Xu"
-					}
-				],
-				["first_name", "last_name", "salt", "hashed_password"]
-			);
+  it("should return an administrator", async () => {
+    mock
+      .expects("query")
+      .once()
+      .withArgs(
+        "SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1"
+      )
+      .callsArgWith(
+        2,
+        null,
+        [
+          {
+            first_name: "Admin",
+            last_name: "",
+            salt: "$2b$10$z1e0ySIYbA/5FXNzZy.Qge",
+            hashed_password:
+              "$2b$10$z1e0ySIYbA/5FXNzZy.Qgefti4GC8YwobSbO81EfD9JTuX/X1J1Xu",
+          },
+        ],
+        ["first_name", "last_name", "salt", "hashed_password"]
+      );
 
-		expect(await repository.getAdministrator(email)).toBeDefined()
-	});
+    expect(await repository.getAdministrator(email)).toBeDefined();
+  });
 
-	it("should return an error if failed get an administrator", async () => {
-		mock.expects("query").once().callsArgWith(2, new Error(), null, null);
+  it("should return an error if failed get an administrator", async () => {
+    mock.expects("query").once().callsArgWith(2, new Error(), null, null);
 
-		try {
-			await repository.getAdministrator(email);
-		} catch (err) {
-			expect(err).toBeInstanceOf(Error)
-		}
-	})
+    try {
+      await repository.getAdministrator(email);
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
 
-	// it("should delete an author", async () => {
-	// 	mock.expects("query").once().withArgs("DELETE FROM users")
+  it("should return an author", async () => {
+    mock
+      .expects("query")
+      .once()
+      .withArgs(
+        "SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS FALSE LIMIT 1"
+      )
+      .callsArgWith(
+        2,
+        null,
+        [
+          {
+            first_name: "author",
+            last_name: "",
+            salt: "$2b$10$z1e0ySIYbA/5FXNzZy.Qge",
+            hashed_password:
+              "$2b$10$z1e0ySIYbA/5FXNzZy.Qgefti4GC8YwobSbO81EfD9JTuX/X1J1Xu",
+          },
+        ],
+        ["first_name", "last_name", "salt", "hashed_password"]
+      );
 
-	// 	try {
-	// 		await repository.deleteAuthor(email)
-	// 	} catch(err) {
-	// 		expect(err).toBeUndefined()
-	// 	}
-	// })
-	
-	// it("should return an error if failed delete an author", async () => {
-	// 	mock.expects("query").once().callArgWith(2, new Error(), null, null);
-	
-	// 	try {
-	// 		await repository.deleteAuthor(email)
-	// 	} catch (err) {
-	// 		expect(err).toBeInstanceOf(Error)
-	// 	}
-	// })
+    expect(await repository.getAuthor(email)).toBeDefined();
+  });
+
+  it("should return an error if failed get an author", async () => {
+    mock.expects("query").once().callsArgWith(2, new Error(), null, null);
+
+    try {
+      await repository.getAuthor(email);
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
+
+  // it("should delete an author", async () => {
+  // 	mock.expects("query").once().withArgs("DELETE FROM users")
+
+  // 	try {
+  // 		await repository.deleteAuthor(email)
+  // 	} catch(err) {
+  // 		expect(err).toBeUndefined()
+  // 	}
+  // })
+
+  // it("should return an error if failed delete an author", async () => {
+  // 	mock.expects("query").once().callArgWith(2, new Error(), null, null);
+
+  // 	try {
+  // 		await repository.deleteAuthor(email)
+  // 	} catch (err) {
+  // 		expect(err).toBeInstanceOf(Error)
+  // 	}
+  // })
 });
--- src/infrastructure/database/mysql/UserRepository.ts
+++ src/infrastructure/database/mysql/UserRepository.ts
@@ -7,76 +7,94 @@ import Name from "../../../domain/valueobject/Name";
 import Password from "../../../domain/valueobject/Password";
 
 export default class UserRepository implements UserRepositoryInterface.default {
-	private _connection: Connection;
+  private _connection: Connection;
 
-	constructor(connection: Connection) {
-		this._connection = connection;
-	}
+  constructor(connection: Connection) {
+    this._connection = connection;
+  }
 
-	public getAdministrator(email: Email): Promise<Administrator> {
-		return new Promise<Administrator>((resolve, reject) => {
-			this._connection.query(
-				"SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1",
-				[email.string()],
-				(err: any | null, result: any) => {
-					if (err) reject(err);
-					if (result.length > 0) {
-						resolve(new Administrator(
-							email,
-							new Name(result[0].first_name, result[0].last_name),
-							new Password(result[0].salt, result[0].hashed_password)
-						))
-					}
-				}
-			);
-		});
-	}
+  public getAdministrator(email: Email): Promise<Administrator> {
+    return new Promise<Administrator>((resolve, reject) => {
+      this._connection.query(
+        "SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS TRUE LIMIT 1",
+        [email.string()],
+        (err: any | null, result: any) => {
+          if (err) reject(err);
+          if (result.length > 0) {
+            resolve(
+              new Administrator(
+                email,
+                new Name(result[0].first_name, result[0].last_name),
+                new Password(result[0].salt, result[0].hashed_password)
+              )
+            );
+          }
+        }
+      );
+    });
+  }
 
-	public getAuthor(email: Email): Promise<Author> {
-		return new Promise<Author>(() => { });
-	}
+  public getAuthor(email: Email): Promise<Author> {
+    return new Promise<Author>((resolve, reject) => {
+      this._connection.query(
+        "SELECT first_name, last_name, salt, hashed_password FROM users WHERE email = ? AND is_administrator IS FALSE LIMIT 1",
+        [email.string()],
+        (err: any | null, result: any) => {
+          if (err) reject(err);
+          if (result.length > 0) {
+            resolve(
+              new Author(
+                email,
+                new Name(result[0].first_name, result[0].last_name),
+                new Password(result[0].salt, result[0].hashed_password)
+              )
+            );
+          }
+        }
+      );
+    });
+  }
 
-	public saveAuthor(author: Author): Promise<void> {
-		return new Promise<void>(() => { });
-	}
+  public saveAuthor(author: Author): Promise<void> {
+    return new Promise<void>(() => {});
+  }
 
-	public updateAdministrator(administrator: Administrator): Promise<void> {
-		return new Promise<void>((resolve, reject) => {
-			
-			this._connection.query(
-				"UPDATE users SET email = ?, first_name = ?, last_name = ?, salt = ?, hashed_password = ? WHERE email = ? AND is_administrator IS TRUE LIMIT 1",
-				[
-					administrator.email,					
-					administrator.name.first,
-					administrator.name.last,
-					administrator.password.salt,
-					administrator.password.hashedPassword,
-					administrator.email.string(),
-				],
-				(err: any | null, result: any) => {
-					if (err) reject(err)
+  public updateAdministrator(administrator: Administrator): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this._connection.query(
+        "UPDATE users SET email = ?, first_name = ?, last_name = ?, salt = ?, hashed_password = ? WHERE email = ? AND is_administrator IS TRUE LIMIT 1",
+        [
+          administrator.email,
+          administrator.name.first,
+          administrator.name.last,
+          administrator.password.salt,
+          administrator.password.hashedPassword,
+          administrator.email.string(),
+        ],
+        (err: any | null, result: any) => {
+          if (err) reject(err);
 
-					resolve(result)
-				}
-			)
-		});
-	}
+          resolve(result);
+        }
+      );
+    });
+  }
 
-	public updateAuthor(author: Author): Promise<void> {
-		return new Promise<void>(() => { });
-	}
+  public updateAuthor(author: Author): Promise<void> {
+    return new Promise<void>(() => {});
+  }
 
-	public deleteAuthor(email: Email): Promise<void> {
-		return new Promise<void>((resolve, reject) => { 
-			this._connection.query(
-				"DELETE FROM users WHERE is_administrator = FALSE AND email = ?",
-				[email.string()],
-				(err: any | null, result: any) => {
-					if (err) reject(err)
+  public deleteAuthor(email: Email): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this._connection.query(
+        "DELETE FROM users WHERE is_administrator = FALSE AND email = ?",
+        [email.string()],
+        (err: any | null, result: any) => {
+          if (err) reject(err);
 
-					resolve(result)
-				}
-			)
-		});
-	}
+          resolve(result);
+        }
+      );
+    });
+  }
 }
