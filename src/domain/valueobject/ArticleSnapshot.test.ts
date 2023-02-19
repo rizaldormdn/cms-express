1a8761869346263f0ff2a64cd1f26999678ffe80
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-19 21:55:27 +0700

    test: ArticleSnapshot

--- src/domain/valueobject/ArticleSnapshot.test.ts
+++ src/domain/valueobject/ArticleSnapshot.test.ts
@@ -1,39 +1,28 @@
-import ArticleSnapshot from "./ArticleSnapshot";
-import Slug from "./Slug";
-import Tag, { Tags } from "./Tag";
-import ArticleDate from "./ArticleDate";
+import { slug, title, excerpt, imageThumbnailURL, authorName, tags, articleDate, articleSnapshot } from "../../../test/data/Data"
+import ArticleDate from "./ArticleDate"
+import ArticleSnapshot from "./ArticleSnapshot"
 
-type articleSnapshot = {
+type articleSnapshotParam = {
   title: string
   excerpt: string
   imageThumbnailURL: string
   authorName: string
 }
-type articleSnapshots = articleSnapshot[]
+type articleSnapshotParams = articleSnapshotParam[]
 
 describe("ArticleSnapshot", () => {
-  let slug = new Slug("this-is-title-abc123")
-  let title = "This is title"
-  let excerpt = "This is excerpt"
-  let imageThumbnailURL = "http://example.com/thumbnail.jpg"
-  let authorName = "John Doe"
-  let tags: Tags = [new Tag("Tag 1"), new Tag("Tag 2"), new Tag("Tag 3")]
-  let date = new ArticleDate()
-
   it("should have slug, title, excerpt, image thumbnail URL, author name, tags, and date", () => {
-    let articleSnapshot = new ArticleSnapshot(slug, title, excerpt, imageThumbnailURL, authorName, tags, date)
-
     expect(articleSnapshot.slug).toBe(slug)
     expect(articleSnapshot.title).toBe(title)
     expect(articleSnapshot.excerpt).toBe(excerpt)
     expect(articleSnapshot.imageThumbnailURL).toBe(imageThumbnailURL)
     expect(articleSnapshot.authorName).toBe(authorName)
     expect(articleSnapshot.tags).toBe(tags)
-    expect(articleSnapshot.date).toBe(date)
+    expect(articleSnapshot.date).toBe(articleDate)
   })
 
   test("title, excerpt, image thumbnail URL, and author name cannot be empty", () => {
-    let entries: articleSnapshots = [
+    let params: articleSnapshotParams = [
       {
         title: "",
         excerpt: excerpt,
@@ -60,15 +49,15 @@ describe("ArticleSnapshot", () => {
       }
     ]
 
-    for (let entry of entries) {
+    for (let param of params) {
       expect(() => new ArticleSnapshot(
         slug,
-        entry.title,
-        entry.excerpt,
-        entry.imageThumbnailURL,
-        entry.authorName,
+        param.title,
+        param.excerpt,
+        param.imageThumbnailURL,
+        param.authorName,
         tags,
-        date
+        new ArticleDate()
       )).toThrowError()
     }
   })
--- test/data/Data.ts
+++ test/data/Data.ts
@@ -8,14 +8,21 @@ import ImageURL from "../../src/domain/valueobject/ImageURL";
 import Image from "../../src/domain/entity/Image";
 import Article from "../../src/domain/aggregate/Article";
 import Tag, { Tags } from "../../src/domain/valueobject/Tag";
+import Slug from "../../src/domain/valueobject/Slug";
+import ArticleDate from "../../src/domain/valueobject/ArticleDate";
+import ArticleSnapshot from "../../src/domain/valueobject/ArticleSnapshot";
 
 export const email: Email = new Email("test@example.com");
 export const name: Name = new Name("John Doe");
+export const authorName = "John Doe"
 export const password: Password = new Password("$2b$10$WCZ6j4PLICecyCYvBvL7We");
 export const author: Author = new Author(email, name, password);
-export const content = new Content("This is title", "This is content", "This is excerpt")
+export const title = "This is title"
+export const excerpt = "This is excerpt."
+export const content = new Content(title, "<p>This is content.</p>", excerpt)
 export const dimension = new Dimension(1920, 1080)
-export const imageURL = new ImageURL("http://example.com/original.jpg", "http://example.com/thumbnail.jpg")
+export const imageThumbnailURL = "http://example.com/thumbnail.jpg"
+export const imageURL = new ImageURL("http://example.com/original.jpg", imageThumbnailURL)
 export const image = new Image(imageURL, "A sample image", dimension)
 export const tags: Tags = [
   new Tag("tag1"),
@@ -23,3 +30,6 @@ export const tags: Tags = [
   new Tag("tag3")
 ]
 export const article: Article = author.addArticle(content, image, tags)
+export const slug = new Slug("this-is-title-abc123")
+export const articleDate = new ArticleDate()
+export const articleSnapshot = new ArticleSnapshot(slug, title, excerpt, imageThumbnailURL, authorName, tags, articleDate)
