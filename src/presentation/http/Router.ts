dddc2c6511438eaeb9a918b754b76f9ebb61d265
mahrus.rohisyam <mahrusaim@gmail.com>
2023-02-19 11:08:41 +0700

    fix: presentation; app, router and handler

--- src/presentation/http/Router.ts
+++ src/presentation/http/Router.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import PingHandler from './handler/PingHandler';
+import UserHandler from './handler/UserHandler';
+import { Connection } from 'mysql2';
 
-export default (): Router => {
+export default (connection: Connection): Router => {
   const router: Router = Router();
 
   router.use('/v1', PingHandler())
+  router.use('/v2', UserHandler(connection))
 
   return router;
 }
--- src/presentation/http/handler/UserHandler.ts
+++ src/presentation/http/handler/UserHandler.ts
@@ -4,59 +4,74 @@ import mysql, { Connection } from "mysql2";
 import Email from "../../../domain/valueobject/Email";
 require("dotenv").config();
 
-export default (): Router => {
-  const connection: Connection = mysql.createConnection({
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_USERPASS,
-    database: process.env.DB_NAME,
-  });
+export default (connection: Connection): Router => {
+	const router = Router();
 
-  const router = Router();
+	router.post("/login", (req, res) => {
+		const { email, password } = req.body;
 
-  router.get("/me", (req: Request, res: Response) => {
-    let user = new UserRepository(connection);
-    let { email } = req.body;
-    user
-      .getAuthor(email)
-      .then((data) => {
-        res
-          .status(200)
-          .json({
-            status: "success get author",
-            data: {
-              method: req.method,
-              url: req.url,
-              data: data,
-            },
-          })
-          .end();
-      })
-      .catch((err) => {
-        res
-          .status(404)
-          .json({
-            status: "failed get author",
-            data: {
-              method: req.method,
-              url: req.url,
-              error: err,
-            },
-          })
-          .end();
-      });
+		try {
+			res.status(200).json({
+				status: "success",
+				message: "Login successful!",
+				data: { email },
+			}).end();
+		} catch (error) {
+			res
+				.status(400)
+				.json({
+					status: "error",
+					message: "Invalid Credential!",
+					data: { email, password },
+				})
+				.end();
+		}
+	});
 
-    res
-      .status(200)
-      .json({
-        status: "success",
-        message: "server is alive",
-        data: {
-          method: req.method,
-          url: req.url,
-        },
-      })
-      .end();
-  });
+	router.get("/me", (req: Request, res: Response) => {
+		let user = new UserRepository(connection);
+		let { email } = req.body;
+		user
+			.getAuthor(email)
+			.then((data) => {
+				res
+					.status(200)
+					.json({
+						status: "success get author",
+						data: {
+							method: req.method,
+							url: req.url,
+							data: data,
+						},
+					})
+					.end();
+			})
+			.catch((err) => {
+				res
+					.status(404)
+					.json({
+						status: "failed get author",
+						data: {
+							method: req.method,
+							url: req.url,
+							error: err,
+						},
+					})
+					.end();
+			});
 
-  return router;
+		res
+			.status(200)
+			.json({
+				status: "success",
+				message: "server is alive",
+				data: {
+					method: req.method,
+					url: req.url,
+				},
+			})
+			.end();
+	});
+
+	return router;
 };
