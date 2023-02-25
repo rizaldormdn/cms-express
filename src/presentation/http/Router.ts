fc1c8555f4a368eadfa32662f4fa9e54209d1d61
ari-salt <aristo.rinjuang@salt.co.id>
2023-02-25 16:09:22 +0700

    fix: conflicts on routers

--- src/presentation/http/Router.ts
+++ src/presentation/http/Router.ts
@@ -9,13 +9,8 @@ export default (connection: Connection): Router => {
   const router: Router = Router();
 
   router.use('/v1', PingHandler())
-<<<<<<< HEAD
-  router.use('/v2', UserHandler(connection))
-  router.use('/v3', ImageHandler(connection))
-=======
   router.use('/v1', ArticleHandler(connection))
   router.use('/v1', UserHandler(connection))
->>>>>>> 80447ab0e0bf4f2ec1a243d3c09df595e3e58bb4
 
   return router;
 }
